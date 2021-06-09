fa3a43c066092440bbeda63597c8aab9f7276035
Ojus Singhal <ojus.singhal17@gmail.com>
2021-06-10 01:11:10 +0530

    remove extra mappings, remove edithackathons.html

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -17,22 +17,4 @@ public class HomeController
         mv.setViewName("index.html");
         return mv;
     }
-    
-    @RequestMapping("addHackathon")
-    public ModelAndView addHackathon(@RequestParam("hackathon") String hackathon)
-    {
-        String s = "hackathon added: " + hackathon;
-        System.out.println(s);
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("editHackathons.html");
-        mv.addObject("editHackathon", s);
-        return mv;
-    }
-    
-    @RequestMapping("removeHackathon")
-    public String removeHackathon(@RequestParam("hackathon") String hackathon)
-    {
-        System.out.println("hackathon removed: " + hackathon);
-        return "index.html";
-    }
 }
